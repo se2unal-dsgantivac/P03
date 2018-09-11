c67ef9e0cf3f2d7c96eadc8e37bf788974e74209
David Santiago Gantiva Castro <dsgantivac@unal.edu.co>
2018-09-10 19:51:59 -0500

    second and final commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../Resources/logo.svg';
+import '../Styles/App.css';
 
 class App extends Component {
   render() {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Dsgantivac 1032494203</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          ctr-alt-elite
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Game from './Components/Game'
+import App from './Components/App'
 import './Styles/index.css'
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 ReactDOM.render(
   <Game />,
-  document.getElementById('root')
+  document.getElementById('root2')
 );
